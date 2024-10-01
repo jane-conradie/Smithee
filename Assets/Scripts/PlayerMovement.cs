a92d144881c2747d310137835e3706f9b2bbe953
Jané Conradie <jconradie1998@gmail.com>
2024-10-01 08:20:17 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+
+   PlayerControls controls;
+   Vector2 moveInput;
+
+   void Awake() 
+   {
+        controls = new PlayerControls();
+        controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+   }
+
+    void OnEnable() 
+    {
+        controls.Player.Enable();
+    }
+
+    void OnDisable() 
+    {
+        controls.Player.Disable();
+    }
+
+    void Update() 
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector2 movement = new Vector2(moveInput.x, moveInput.y) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+}
