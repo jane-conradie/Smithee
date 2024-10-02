f225cb1cceb9c721b1295c0587ef84a611ed8e4b
jane_conradie <jconradie1998@gmail.com>
2024-10-02 09:11:58 +0200

    implementing showing customer status thoughts

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Customer : MonoBehaviour
 {
+    [SerializeField] List<CustomerStatusSO> statuses;
+
     public float moveSpeed;
 
     // pathfinding
@@ -33,14 +36,21 @@ public class Customer : MonoBehaviour
     IEnumerator MoveAlongPath()
     {
         isMoving = true;
+        Transform waypoint = waypoints[waypointIndex];
 
-        Vector3 targetPosition = waypoints[waypointIndex].position;
+        Vector3 targetPosition = waypoint.position;
 
         // moves the object until the target has been reached
         while (Vector2.Distance(transform.position, targetPosition) > 0.01f)
         {
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime * moveSpeed);
             yield return null;
+        }
+
+        if (waypoint.gameObject.tag == "Checkpoint")
+        {
+            // trigger buying of item
+            BuyItem();
         }
 
         // check if at last waypoint
@@ -54,5 +64,56 @@ public class Customer : MonoBehaviour
         }
 
         isMoving = false;
+    }
+
+    // reach destination, buy item
+    // in love emojie on head
+    // move to cashier
+
+    // TO DO mood system that drains the longer the player takes to help them
+
+    void BuyItem()
+    {
+        StartCoroutine(DisplayStatus("Positive"));
+
+        // TO DO grabbing sound
+
+        // TO DO grabbing animation
+
+        // TO DO move to cashier
+
+        // TO DO pay status
+
+        // TO DO
+    }
+
+    IEnumerator DisplayStatus(string sentiment)
+    {
+        // get random positive sprite for status
+        Sprite sprite = GetRandomStatusSpriteBasedOnSentiment(sentiment);
+
+        // get the status holder
+        GameObject statusHolder = transform.Find("Status Holder").gameObject;
+
+        // change the sprite of the sprite rendered to the random sprite
+        GameObject statusPiece = statusHolder.transform.Find("Status Piece").gameObject;
+        SpriteRenderer sr = statusPiece.GetComponent<SpriteRenderer>();
+        sr.sprite = sprite;
+
+        // set the status holder to active
+        statusHolder.SetActive(true);
+
+        yield return new WaitForSecondsRealtime(5);
+
+        statusHolder.SetActive(false);
+
+        yield return null;
+    }
+
+    Sprite GetRandomStatusSpriteBasedOnSentiment(string sentiment)
+    {
+        CustomerStatusSO status = statuses.FirstOrDefault((x) => x.GetSentiment() == sentiment);
+
+        return status.GetRandomSprite();
     }
 }
---
+++ Assets/Scripts/Customer/CustomerStatusSO.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Create New Customer Status", fileName = "Customer Status")]
+public class CustomerStatusSO : ScriptableObject
+{
+    [SerializeField] List<Sprite> sprites;
+    [SerializeField] string sentiment;
+    [SerializeField] string statusType;
+
+    public List<Sprite> GetSprites()
+    {
+        return sprites;
+    }
+
+    public string GetSentiment()
+    {
+        return sentiment;
+    }
+
+    public string GetStatusType()
+    {
+        return statusType;
+    }
+
+    public Sprite GetRandomSprite()
+    {
+        return sprites[Random.Range(0, sprites.Count)];
+    }
+}
